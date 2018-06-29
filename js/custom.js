8b6cbf20a3ee72db987d0d707c8e524b8334fe7a
bishoy <bishoyaziz93i@gmail.com>
2018-06-29 20:20:40 +0200

    r

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,7 @@
 
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
+$(function(){
 	$('.carousel').carousel({
 		interval: 5000
 	})
@@ -12,30 +13,5 @@
 	$(function() {
 		'use strict';
 		$('html').niceScroll();   
-	});
-
-
-
-var scrollButton = $(".scroll");
-
-$(window).scroll(function () {
-    'use strict';
-	
-    if ($(this).scrollTop() >= 600) {
-            
-        scrollButton.show();
-            
-    } else {
-            
-        scrollButton.hide();
-    }
-});
-
-// Click On Button To Scroll Top
-    
-scrollButton.click(function () {
-    'use strict';
-
-    $("html,body").animate({ scrollTop : 0 }, 600);
-        
+    });
 });
